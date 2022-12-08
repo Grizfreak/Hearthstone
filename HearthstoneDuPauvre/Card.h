1c7bbf4fed291195e177b4cd16b273b47aec1c70
AllanLegacy <allanplanchenault@gmail.com>
2022-12-08 10:53:48 +0100

    Mofication Virtual devant useOn dans Card.h

--- HearthstoneDuPauvre/Card.h
+++ HearthstoneDuPauvre/Card.h
@@ -11,7 +11,7 @@ private:
 
 public:
 	Card(std::string name, int attack, int costMana);
-	void useOn(Card card);
-	void useOn(std::vector<Card> listOfCards);
+	virtual void useOn(Card card) = 0;
+	virtual void useOn(std::vector<Card> listOfCards) = 0;
 	~Card();
 };
