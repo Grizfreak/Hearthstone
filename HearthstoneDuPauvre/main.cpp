edb3bc584ccd5fde9ea4f7a22856269f7d96ad18
Antonin Aubert <antonin.aubert0@gmail.com>
2022-12-09 10:34:56 +0100

    Corrected commit

--- HearthstoneDuPauvre/main.cpp
+++ HearthstoneDuPauvre/main.cpp
@@ -7,6 +7,26 @@
 #include "Minion.h"
 #include "Spell.h"
 
+void testConsole() {
+	/* Test for Deck */
+	Spell* spell = new Spell("Spell1", 1, 1, TypeOfSpell::DAMAGE_BOOST);
+	std::vector<Card*> deckForPlayer1{ new Minion("Minion1", 1, 1, 1), new Minion("Minion2", 2, 2, 2), new Minion("Minion3", 3, 3, 3), spell };
+	Deck* deck = new Deck(deckForPlayer1);
+	deck->shuffle();
+	for (int i = 0; i < 4; i++) {
+		std::cout << *(deck->getOneCard());
+	}
+	deck->shuffle();
+
+	/* Test for a minion useOn another minion */
+	Minion* minion1 = new Minion("Minion1", 1, 1, 1);
+	Minion* minion2 = new Minion("Minion2", 2, 2, 2);
+	minion1->useOn(minion2);
+	std::cout << *minion2;
+	std::cout << *minion1;
+	std::cout << *spell;
+}
+
 int main()
 {
     testConsole();
@@ -131,22 +151,4 @@ int main()
 }
 
 
-void testConsole() {
-	/* Test for Deck */
-	Spell* spell = new Spell("Spell1", 1, 1, TypeOfSpell::DAMAGE_BOOST);
-	std::vector<Card*> deckForPlayer1{ new Minion("Minion1", 1, 1, 1), new Minion("Minion2", 2, 2, 2), new Minion("Minion3", 3, 3, 3), spell};
-	Deck *deck = new Deck(deckForPlayer1);
-	deck->shuffle();
-	for (int i = 0; i < 4; i++) {
-		std::cout << *(deck->getOneCard());
-	}
-	deck->shuffle();
-	
-	/* Test for a minion useOn another minion */
-	Minion* minion1 = new Minion("Minion1", 1, 1, 1);
-	Minion* minion2 = new Minion("Minion2", 2, 2, 2);
-	minion1->useOn(minion2);
-	std::cout << *minion2;
-	std::cout << *minion1;
-	std::cout << *spell;
-}
+
