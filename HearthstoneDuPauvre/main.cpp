50fdd1d71eff1cc0f32dc85bac67d6446076d30e
Antonin Aubert <antonin.aubert0@gmail.com>
2022-12-09 10:10:27 +0100

    hello

---
+++ HearthstoneDuPauvre/main.cpp
@@ -0,0 +1,127 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 720), "SFML works!");
+    sf::Texture background;
+    bool holdingCard = false;
+    sf::Vector2i starting_position;
+    starting_position = sf::Mouse::getPosition(window);
+    sf::Vector2f current_position;
+	bool isDragging = false;
+    /*sf::Music theme;
+    if (!theme.openFromFile("Hearthstone_Heroes_of_WarCraft_Main-Title.wav")) {
+        return EXIT_FAILURE;
+    }
+    theme.play();
+	theme.setAttenuation(0.1f);*/
+	if (!background.loadFromFile("./assets/backgrounds/background.jpg"))
+	{
+		// error...
+		return EXIT_FAILURE;
+	}
+	sf::RectangleShape card1(sf::Vector2f(120.f, 140.f));
+    sf::RectangleShape card2(sf::Vector2f(120.f, 140.f));
+	sf::RectangleShape cardOnBoardBox(sf::Vector2f(800.f, 140.f));
+	sf::RectangleShape cardInHand(sf::Vector2f(350.f, 140.f));
+    std::vector<sf::RectangleShape*> playersCardsInHand = { &card1,&card2 };
+	std::vector<sf::RectangleShape*> playersCardsOnBoard = { };
+	std::vector<sf::RectangleShape*> hitboxes = { &cardOnBoardBox, &cardInHand };
+    sf::Sprite s(background);
+	cardOnBoardBox.setPosition(100, 350);
+	cardOnBoardBox.setFillColor(sf::Color::Transparent);
+	cardOnBoardBox.setOutlineColor(sf::Color::Red);
+	cardOnBoardBox.setOutlineThickness(5.f);
+	cardInHand.setPosition(350, 600);
+	cardInHand.setFillColor(sf::Color::Transparent);
+	cardInHand.setOutlineColor(sf::Color::Green);
+	cardInHand.setOutlineThickness(5.f);
+	card1.setPosition(350, 600);
+    card2.setPosition(450, 600);
+	sf::RectangleShape* selectedCard = nullptr;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape) window.close();
+
+            if (event.type == sf::Event::MouseMoved && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+				for (int i = 0; i < playersCardsInHand.size(); i++)
+				{
+					if ((*playersCardsInHand[i]).getGlobalBounds().contains(event.mouseMove.x, event.mouseMove.y) && !holdingCard)
+					{
+                        holdingCard = true;
+						selectedCard = playersCardsInHand[i];
+					}
+				}
+				if (holdingCard)
+				{
+					current_position.x = event.mouseMove.x - 60.f;
+					current_position.y = event.mouseMove.y - 70.f;
+					(*selectedCard).setPosition(current_position);
+				}
+                
+            }
+			if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left && holdingCard)
+			{
+				holdingCard = false;
+				std::cout << "Released" << std::endl;
+				if (cardOnBoardBox.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+				{
+					std::cout << "On board" << std::endl;
+					playersCardsOnBoard.push_back(selectedCard);
+					playersCardsInHand.erase(std::remove(playersCardsInHand.begin(), playersCardsInHand.end(), selectedCard), playersCardsInHand.end());
+					if (playersCardsOnBoard.size() == 1) {
+						(*selectedCard).setPosition(100, 350);
+					}
+					else {
+						for (int i = 0; i < playersCardsOnBoard.size(); i++)
+						{
+							(*playersCardsOnBoard[i]).setPosition(100 + (i * 100), 350);
+						}
+					}
+					
+				}
+				else
+				{
+					std::cout << "Else" << std::endl;
+					if (playersCardsInHand.size() == 1)
+					{
+						(*playersCardsInHand[0]).setPosition(350, 600);
+					}
+					else
+					{
+						for (int i = 0; i < playersCardsInHand.size(); i++)
+						{
+							(*playersCardsInHand[i]).setPosition(350 + (i * 100), 600);
+						}
+					}
+				}
+			}
+        }
+
+        window.clear();
+		window.draw(s);
+		for (int i = 0; i < playersCardsInHand.size(); i++)
+		{
+			window.draw((*playersCardsInHand[i]));
+		}
+		for (int i = 0; i < playersCardsOnBoard.size(); i++)
+		{
+			window.draw((*playersCardsOnBoard[i]));
+		}
+		for (int i = 0; i < hitboxes.size(); i++)
+		{
+			window.draw((*hitboxes[i]));
+		}
+        window.display();
+    }
+
+    return 0;
+}
